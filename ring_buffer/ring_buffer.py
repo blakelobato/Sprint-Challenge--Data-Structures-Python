3186a56a391ce9b6dbb449265b65ba0fd199a94a
Blake Lobato <blakelobato@Blaks-MBP.lan>
2020-09-27 20:20:10 -0700

    all sprint challenges met

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.container = []
+        self.overflow = 0
 
     def append(self, item):
-        pass
+        # check if container full, if not appended normal
+        if len(self.container) < self.capacity:
+            self.container.append(item)
+        # else, replace the item at the overflow index, and increment overflow
+        elif len(self.container) == self.capacity:
+            self.container[self.overflow] = item
+            if self.overflow == self.capacity-1:
+                self.overflow = 0
+            else:
+                self.overflow += 1
 
     def get(self):
-        pass
+        return self.container
